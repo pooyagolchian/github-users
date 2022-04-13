8fe6bd7f2b99202a51cfc7525d877ffc961f9822
pooya golchian <pooya.golchian@gmail.com>
2022-04-13 20:50:43 +0400

    update users reducers

--- src/store/users.ts
+++ src/store/users.ts
@@ -15,16 +15,16 @@ const usersSlice = createSlice({
   name: 'users',
   initialState: initialUsersState,
   reducers: {
-    setUsers(state: UserState, action: { payload: User[] }) {
+    setUsers(state: UserState, action: { payload: User[] }): void {
       state.users = action.payload
     },
-    removeUser(state: UserState, action: { payload: number }) {
+    removeUser(state: UserState, action: { payload: number }): void {
       state.users.splice(
         state.users.findIndex((user: User) => user.id === action.payload),
         1
       )
     },
-    setLoader(state: UserState, action: { payload: boolean }) {
+    setLoader(state: UserState, action: { payload: boolean }): void {
       state.isLoading = action.payload
     },
   },
